c096667a074214805603a4ba089adc9afc91cf58
baoruichen <1152383857@qq.com>
2022-02-07 10:51:59 +0800

    Create Program2.py

---
+++ assignment2/Program2.py
@@ -0,0 +1,14 @@
+money = float(input("This program will project how much you will owe the bank at the end of each year assuming that you have an interest only mortgage.\nTo begin, enter the initial size of the mortgage: "))
+rate = float(input("Next, enter the annual interest rate: "))
+interest = money*rate/100
+
+print("Your maximum monthly payment will be",format(interest/12,",.2f"))
+
+payment = 12*float(input("Now, enter your planned monthly payment: "))
+
+print("Year  Starting Balance   Interest   Payment   Ending Balance")
+
+for i in range(3):
+    print(format(i+1,"<6d"),format(money,"<19.1f"),format(interest,"<11,.2f"),format(payment,"<10,.2f"),format(money+interest-payment,"<,.2f"),sep="")
+    money = money+interest-payment
+    interest = money*rate/100
