72c15a389c5035d2fe0136d4bde5934f03eaef0f
baoruichen <1152383857@qq.com>
2022-02-07 10:59:23 +0800

    Update Program2.py

--- assignment2/Program2.py
+++ assignment2/Program2.py
@@ -12,3 +12,6 @@ for i in range(3):
     print(format(i+1,"<6d"),format(money,"<19.1f"),format(interest,"<11,.2f"),format(payment,"<10,.2f"),format(money+interest-payment,"<,.2f"),sep="")
     money = money+interest-payment
     interest = money*rate/100
+
+#如果课程内容还没有讲到循环，可以将for循环拆成三个条语句
+#其中i的取值为0,1,2
